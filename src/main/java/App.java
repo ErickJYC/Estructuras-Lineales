8652aa5a922ac71c45d7981e87711bb4c05f96b5
Erick Yunga <eyungac3@est.ups.edu.ec>
2025-06-05 07:35:48 -0500

    feat(UPS-004): se ha agregado el findByName y el removeByName

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -27,16 +27,32 @@ public class App {
         Persona p1 = new Persona("Pablo");
         Persona p2 = new Persona("Juan");
         Persona p3 = new Persona("Maria");
+        Persona p4 = new Persona("David");
 
         ColaG<Persona> colaPersonas = new ColaG<Persona>();
         colaPersonas.add(p1);
         colaPersonas.add(p2);
         colaPersonas.add(p3);
+        colaPersonas.add(p4);
         colaPersonas.printCola();
         System.out.println(colaPersonas.size());
         colaPersonas.printCola();
         System.out.println("Atiende a la persona ->" + colaPersonas.remove());
         System.out.println(colaPersonas.size());;
         System.out.println("tam = "+ colaPersonas.size());
+
+//        String nombreBuscar = "David";
+        colaPersonas.findByName("David");
+        Persona pEliminada = colaPersonas.removeByName("David");
+       if (pEliminada != null){
+           System.out.println("Persona eliminada -> " + pEliminada.getNombre());
+       }else {
+           System.out.println("No se encontro a " + colaPersonas+ " en la cola ");
+//           System.out.println("No se encontro a " + nombreBuscar + " en la cola ");
+
+       }
+
+
+
     }
 }
--- src/main/java/controllers/ColaG.java
+++ src/main/java/controllers/ColaG.java
@@ -1,6 +1,6 @@
 package controllers;
 
-import models.Node;
+import models.Persona;
 import models.NodeGenerico;
 
 import java.util.EmptyStackException;
@@ -59,6 +59,51 @@ public class ColaG <T>{
     }
     public int size(){
         return size;
+    }
+    public int findByName(String nombre) {
+        int posicion = 0;
+        NodeGenerico<T> current = primero;
+        while (current != null) {
+            if (current.getValue().toString().equals(nombre)) {
+                return posicion;
+            }
+            current = current.getNext();
+        }
+        return -1;
+    }
+
+    public Persona removeByName(String nombre) {
+        if (isEmpty()){
+            throw new EmptyStackException();
+        }
+        NodeGenerico<T> actual = primero;
+        NodeGenerico<T> anterior = null;
+
+        while (actual != null){
+            T valor = actual.getValue();
+            if (valor instanceof Persona) {
+                Persona persona = (Persona) valor;
+                if (persona.getNombre().equalsIgnoreCase(nombre)) {
+                    // Si es el primer nodo
+                    if (anterior == null) {
+                        primero = actual.getNext();
+                        if (primero == null) {
+                            ultimo = null;
+                        }
+                    } else {
+                        anterior.setNext(actual.getNext());
+                        if (actual == ultimo) {
+                            ultimo = anterior;
+                        }
+                    }
+                    size--;
+                    return persona;
+                }
+            }
+            anterior = actual;
+            actual = actual.getNext();
+        }
+        return null; // Si no se encontró
     }
 
 }
